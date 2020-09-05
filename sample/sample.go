5acbb3192e26a9b5dc47afd9654870d90c90737e
Jesper Wrang <jeswr740@student.liu.se>
2020-09-05 13:00:14 +0200

    Simplified demo + remove gh pages stuff

--- sample/sample.go
+++ sample/sample.go
@@ -1,74 +1,43 @@
-// +build !js
-
 package main
 
 import (
-	"fmt"
-	"github.com/goxjs/gl"
-	"github.com/goxjs/glfw"
-	"github.com/shibukawa/nanovgo"
-	"github.com/shibukawa/nanovgo/perfgraph"
-	"github.com/shibukawa/nanovgo/sample/demo"
-	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+
+	"github.com/JubbeArt/nanovgo"
 )
 
-var blowup bool
-var premult bool
-
-func key(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-	if key == glfw.KeyEscape && action == glfw.Press {
-		w.SetShouldClose(true)
-	} else if key == glfw.KeySpace && action == glfw.Press {
-		blowup = !blowup
-	} else if key == glfw.KeyP && action == glfw.Press {
-		premult = !premult
-	}
-}
-
 func main() {
-	err := glfw.Init(gl.ContextWatcher)
-	if err != nil {
-		panic(err)
-	}
+	runtime.LockOSThread()
+
+	err := glfw.Init()
+	check(err)
 	defer glfw.Terminate()
 
+	err = gl.Init()
+	check(err)
 	// demo MSAA
 	glfw.WindowHint(glfw.Samples, 4)
 
 	window, err := glfw.CreateWindow(1000, 600, "NanoVGo", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.SetKeyCallback(key)
+	check(err)
 	window.MakeContextCurrent()
 
 	ctx, err := nanovgo.NewContext(0 /*nanovgo.AntiAlias | nanovgo.StencilStrokes | nanovgo.Debug*/)
+	check(err)
 	defer ctx.Delete()
-
-	if err != nil {
-		panic(err)
-	}
-
-	demoData := LoadDemo(ctx)
 
 	glfw.SwapInterval(0)
 
-	fps := perfgraph.NewPerfGraph("Frame Time", "sans")
-
 	for !window.ShouldClose() {
-		t, _ := fps.UpdateGraph()
-
 		fbWidth, fbHeight := window.GetFramebufferSize()
 		winWidth, winHeight := window.GetSize()
-		mx, my := window.GetCursorPos()
 
 		pixelRatio := float32(fbWidth) / float32(winWidth)
-		gl.Viewport(0, 0, fbWidth, fbHeight)
-		if premult {
-			gl.ClearColor(0, 0, 0, 0)
-		} else {
-			gl.ClearColor(0.3, 0.3, 0.32, 1.0)
-		}
+		gl.Viewport(0, 0, int32(fbWidth), int32(fbHeight))
+		gl.ClearColor(0, 0, 0, 0)
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
 		gl.Enable(gl.BLEND)
 		gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
@@ -77,8 +46,12 @@ func main() {
 
 		ctx.BeginFrame(winWidth, winHeight, pixelRatio)
 
-		demo.RenderDemo(ctx, float32(mx), float32(my), float32(winWidth), float32(winHeight), t, blowup, demoData)
-		fps.RenderGraph(ctx, 5, 5)
+		ctx.BeginPath()
+
+		ctx.SetFillColor(nanovgo.RGB(255, 0, 0))
+		//ctx.SetFillColor(colornames.Red)
+		ctx.Rect(50, 50, 100, 100)
+		ctx.Fill()
 
 		ctx.EndFrame()
 
@@ -86,31 +59,11 @@ func main() {
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
-
-	demoData.FreeData(ctx)
 }
 
-func LoadDemo(ctx *nanovgo.Context) *demo.DemoData {
-	d := &demo.DemoData{}
-	for i := 0; i < 12; i++ {
-		path := fmt.Sprintf("images/image%d.jpg", i+1)
-		d.Images = append(d.Images, ctx.CreateImage(path, 0))
-		if d.Images[i] == 0 {
-			log.Fatalf("Could not load %s", path)
-		}
+func check(err error) {
+	if err != nil {
+		panic(err)
 	}
 
-	d.FontIcons = ctx.CreateFont("icons", "entypo.ttf")
-	if d.FontIcons == -1 {
-		log.Fatalln("Could not add font icons.")
-	}
-	d.FontNormal = ctx.CreateFont("sans", "Roboto-Regular.ttf")
-	if d.FontNormal == -1 {
-		log.Fatalln("Could not add font italic.")
-	}
-	d.FontBold = ctx.CreateFont("sans-bold", "Roboto-Bold.ttf")
-	if d.FontBold == -1 {
-		log.Fatalln("Could not add font bold.")
-	}
-	return d
 }
